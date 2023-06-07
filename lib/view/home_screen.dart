970d1e1753c1e6f5b58ba2a9186e554b11772688
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-07 20:05:38 +0600

    Get API's #5

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_mania/utils/routes/routes_name.dart';
+import 'package:movie_mania/view_model/home_view_model.dart';
 import 'package:movie_mania/view_model/user_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -11,10 +12,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  HomeViewModel homeViewModel = HomeViewModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    homeViewModel.fetchMoviesListApi();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final userPreference = Provider.of<UserViewModel>(context);
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -28,12 +38,13 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Center(child: Text('Logout'))),
         ],
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
+      body: ChangeNotifierProvider<HomeViewModel>(
+        create: (BuildContext context) => homeViewModel,
+        child: Consumer<HomeViewModel>(
+          builder: (context, value, _){
 
-          ],
-        ),
+            return Container();
+          },),
       ),
     );
   }
--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,15 +1,33 @@
 
 import 'package:flutter/material.dart';
+import 'package:movie_mania/data/response/api_response.dart';
+import 'package:movie_mania/model/movies_model.dart';
 import 'package:movie_mania/respository/home_repository.dart';
 
 class HomeViewModel with ChangeNotifier {
 
   final _myRepo = HomeRepository();
 
+  ApiResponse<MovieListModel> moviesList = ApiResponse.loading();
+
+  setMoviesList(ApiResponse<MovieListModel> response){
+    moviesList = response;
+    notifyListeners();
+  }
+
   Future<void> fetchMoviesListApi()async{
+
+    setMoviesList(ApiResponse.loading());
 
     _myRepo.fetchMoviesList().then((value){
 
+      setMoviesList(ApiResponse.completed(value));
+
+    }).onError((error, stackTrace){
+
+      setMoviesList(ApiResponse.error(error.toString()));
+
     });
   }
+
 }
