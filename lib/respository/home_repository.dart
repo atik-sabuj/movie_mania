df819fef34a1f50aecff415b0dbc76c63d91aa2a
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-07 19:41:41 +0600

    Get API's #4

--- lib/respository/home_repository.dart
+++ lib/respository/home_repository.dart
@@ -1,5 +1,23 @@
 
+import 'package:movie_mania/data/network/BaseApiServices.dart';
+import 'package:movie_mania/data/network/NetworkApiServices.dart';
+import 'package:movie_mania/model/movies_model.dart';
+import 'package:movie_mania/res/app_url.dart';
+
 class HomeRepository {
 
-  
+  BaseApiServices _apiServices = NetworkApiService();
+
+  Future<MovieListModel> fetchMoviesList()async{
+
+    try{
+
+      dynamic response = await _apiServices.getGetApiResponse(AppUrl.moviesListEndPoint);
+      return response = MovieListModel.fromJson(response);
+
+    }catch(e){
+      throw e;
+    }
+  }
+
 }
--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,7 +1,15 @@
 
 import 'package:flutter/material.dart';
+import 'package:movie_mania/respository/home_repository.dart';
 
 class HomeViewModel with ChangeNotifier {
 
+  final _myRepo = HomeRepository();
 
+  Future<void> fetchMoviesListApi()async{
+
+    _myRepo.fetchMoviesList().then((value){
+
+    });
+  }
 }
